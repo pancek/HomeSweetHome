dbf36f52e5a621b82fbe5f4b92a1c33f23a539f7
66621 <66621@CEUEKTIUNIV-009.ue.poznan>
2018-04-19 12:45:32 +0200

    pawel-3

--- HomeSweetHome/src/com/lab/hsh/Tuner.java
+++ HomeSweetHome/src/com/lab/hsh/Tuner.java
@@ -1,5 +1,29 @@
 package com.lab.hsh;
 
-public class Tuner {
+public class Tuner implements Device {
+	private void setAm() {
+		// TODO Auto-generated method stub
+		System.out.println("AM freq defined");
+	}
 
+	private void setFm() {
+		// TODO Auto-generated method stub
+		System.out.println("FM freq defined");
+	}
+	
+	private void setFrequency() {
+		// TODO Auto-generated method stub
+		System.out.println("Set desired frequency - am/fm mode");
+	}
+
+	public void on() {
+		System.out.println("Tuner is on");
+		
+	}
+
+	@Override
+	public void off() {
+		System.out.println("Tuner is on");
+		
+	}
 }
