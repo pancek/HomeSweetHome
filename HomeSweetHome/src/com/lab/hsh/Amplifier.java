536a8247de176c389f40d1478de18f16ad0c90dd
47289 <47289@CEUEKTIUNIV-005.ue.poznan>
2018-04-19 12:46:52 +0200

    NEw changes

--- HomeSweetHome/src/com/lab/hsh/Amplifier.java
+++ HomeSweetHome/src/com/lab/hsh/Amplifier.java
@@ -1,5 +1,39 @@
 package com.lab.hsh;
 
-public class Amplifier {
+public class Amplifier implements Device {
 
+private boolean isOn = false;
+	
+	@Override
+	public void on() {
+		isOn = true;
+		System.out.println("Cdplayer is on.");
+	}
+
+	@Override
+	public void off() {
+		isOn = false;
+		System.out.println("Cdplayer is off.");
+	}
+	
+	public void setStereoSound() {
+		System.out.println("Stereo sound set.");
+	}
+	
+	public void setVolume(int level) {
+		System.out.println("Volume set at: "+level);
+	}
+	
+	public void setTuner(Tuner t) {
+		System.out.println("Tuner set.");
+	}
+	
+	public void setDVD(DvdPlayer dvd) {
+		System.out.println("DVD set.");
+	}
+	
+	public void setCD(CdPlayer cd) {
+		System.out.println("CD set.");
+	}
+	
 }
--- HomeSweetHome/src/com/lab/hsh/Tuner.java
+++ HomeSweetHome/src/com/lab/hsh/Tuner.java
@@ -1,5 +1,31 @@
 package com.lab.hsh;
 
-public class Tuner {
+public class Tuner implements Device {
+	
+	private boolean isOn = false;
+	
+	@Override
+	public void on() {
+		isOn = true;
+		System.out.println("Cdplayer is on.");
+	}
+
+	@Override
+	public void off() {
+		isOn = false;
+		System.out.println("Cdplayer is off.");
+	}
+	
+	public void setAM() {
+		System.out.println("AM set");
+	}
+	
+	public void setFM() {
+		System.out.println("FM set");
+	}
+	
+	public void setFrequency() {
+		System.out.println("Frequency set");
+	}
 
 }
