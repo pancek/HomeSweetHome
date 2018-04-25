c90715756f0e528010dcd1aef06fe696d3012f34
Paweł <Pawe@DESKTOP-TIUCCDL>
2018-04-25 17:55:55 +0200

    next

--- HomeSweetHome/src/com/lab/hsh/Lights.java
+++ HomeSweetHome/src/com/lab/hsh/Lights.java
@@ -5,13 +5,13 @@ public class Lights implements Device {
 	@Override
 	public void on() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Lights are on");
 	}
 
 	@Override
 	public void off() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Lights are off");
 	}
 
 }
